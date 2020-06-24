3b35c8181218b4742757d50660b9d374b3cfa9bf
Filip <filip26@gmail.com>
2020-06-24 21:18:11 +0200

    update CompactionApi javadoc

--- src/main/java/com/apicatalog/jsonld/api/builder/CompactionApi.java
+++ src/main/java/com/apicatalog/jsonld/api/builder/CompactionApi.java
@@ -11,6 +11,7 @@ import com.apicatalog.jsonld.document.RemoteDocument;
 import com.apicatalog.jsonld.lang.Version;
 import com.apicatalog.jsonld.loader.LoadDocumentCallback;
 import com.apicatalog.jsonld.processor.CompactionProcessor;
+import com.apicatalog.jsonld.uri.UriUtils;
 
 public final class CompactionApi {
 
@@ -47,6 +48,12 @@ public final class CompactionApi {
         this.options = new JsonLdOptions();
     }
 
+    /**
+     * Override an existing settings with {@link JsonLdOptions}.
+     * 
+     * @param options {@link JsonLdOptions}
+     * @return {@link CompactionApi} instance 
+     */
     public CompactionApi options(JsonLdOptions options) {
         
         if (options == null) {
@@ -57,52 +64,123 @@ public final class CompactionApi {
         return this;
     }
 
+    /**
+     * Set <code>JSON-LD</code> processing mode. JSON-LD 1.1 is set by default.
+     *  
+     * @param processingMode
+     * @return {@link CompactionApi} instance
+     */
     public CompactionApi mode(Version processingMode) {
         options.setProcessingMode(processingMode);
         return this;
     }
 
-    public CompactionApi base(URI baseUri) {
+    /**
+     * Set the base <code>IRI</code>. If set, this overrides the input document's IRI.
+     * 
+     * @param baseUri
+     * @return {@link CompactionApi} instance
+     */
+    public CompactionApi base(URI baseUri) {        
         options.setBase(baseUri);
         return this;
     }
 
+    /**
+     * Set the base {@link URI}. If set, this overrides the input document's IRI.
+     *
+     * @param baseUri
+     * @return {@link CompactionApi} instance
+     */
     public CompactionApi base(String baseUri) {
-        return base(URI.create(baseUri));
+        return base(baseUri != null ? UriUtils.create(baseUri) : null);
     }
 
+    
+    /**
+     * If set to <code>true</code>, the processor replaces arrays with just one
+     * element  If set to false, all arrays will remain arrays even if they have just one
+     * element.
+     *
+     * @param enable 
+     * @return {@link CompactionApi} instance
+     */
     public CompactionApi compactArrays(boolean enable) {
         options.setCompactArrays(enable);
         return this;
     }
 
+    /**
+     * The processor replaces arrays with just one element. 
+     * 
+     * @return {@link CompactionApi} instance
+     */
     public CompactionApi compactArrays() {
         return compactArrays(true);
     }
-
+    
+    /**
+     * Determines if IRIs are compacted relative to the {@link #base(URI)} or document location 
+     * 
+     * @param enable
+     * @return {@link CompactionApi} instance
+     */
     public CompactionApi compactToRelative(boolean enable) {
         options.setCompactToRelative(enable);
         return this;
     }
 
+    /**
+     * IRIs are compacted relative to the {@link #base(URI)} or document location. 
+     * 
+     * @return {@link CompactionApi} instance
+     */
     public CompactionApi compactToRelative() {
         return compactToRelative(true);
     }
 
+    /**
+     * Set the loader to be used to retrieve remote documents and
+     * contexts, implementing the {@link LoadDocumentCallback}. If specified, it is
+     * used to retrieve remote documents and contexts; otherwise, if not specified,
+     * the processor's built-in loader is used.
+     * 
+     * @param loader
+     * @return {@link CompactionApi} instance
+     */
     public CompactionApi loader(LoadDocumentCallback loader) {        
         options.setDocumentLoader(loader);
         return this;
     }
-
+    
+    /**
+     * If set to <code>true</code>, certain algorithm processing steps where
+     * indicated are ordered lexicographically. If <code>false</code>, order is not
+     * considered in processing.
+     * 
+     * @param enable
+     * @return {@link CompactionApi} instance
+     */
     public CompactionApi ordered(boolean enable) {
         options.setOrdered(enable);
         return this;
     }
 
+    /**
+     * Certain algorithm processing steps where indicated are ordered lexicographically.
+     * 
+     * @return {@link CompactionApi} instance
+     */
     public CompactionApi ordered() {
         return ordered(true);
     }
 
+    /**
+     * Get the result of compaction.
+     * 
+     * @return {@link JsonObject} representing a result of compaction
+     * @throws JsonLdError
+     */
     public JsonObject get() throws JsonLdError {
         if (documentUri != null && contextUri != null)  {
             return CompactionProcessor.compact(documentUri, contextUri, options);
