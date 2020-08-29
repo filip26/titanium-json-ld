af4a55727603353b2fb999b93ea5fac7a653f2bb
Jean-Marc Vanel <jeanmarc.vanel@gmail.com>
2020-08-29 17:06:51 +0200

    Related to issue #61, added one test failing with same data and file: URL's
    NOTE:
    The 2 added tests are with expanded context. I don't know whether the plain context and the expanded context are different things for ToRDF().

--- src/test/java/com/apicatalog/jsonld/issue/ToRdfIssue61Test.java
+++ src/test/java/com/apicatalog/jsonld/issue/ToRdfIssue61Test.java
@@ -15,6 +15,8 @@ import com.apicatalog.jsonld.document.JsonDocument;
 import com.apicatalog.jsonld.document.RdfDocument;
 import com.apicatalog.rdf.RdfComparison;
 import com.apicatalog.rdf.RdfDataset;
+import com.apicatalog.rdf.RdfNQuad;
+import com.apicatalog.jsonld.api.JsonLdOptions;
 
 public class ToRdfIssue61Test {
 
@@ -30,8 +32,36 @@ public class ToRdfIssue61Test {
         assertNotNull(result);
 
         try (final InputStream is = getClass().getResourceAsStream("issue61-out.nq")) {
+
+            assertNotNull(is);
+
+        System.out.println("testToRdfMissingTriples");
+        printResult(result);
+
+            boolean match = RdfComparison.equals(result, RdfDocument.of(is).getRdfContent().orElse(null));
+
+            assertTrue(match);
+        }
+    }
+
+    @Test
+    public void testToRdfMissingTriples2() throws JsonLdError, IOException {
+
+        final Document document = readDocument("issue61-in.json");
+        final Document context = readDocument("issue61-context.json");
+        JsonLdOptions options = new JsonLdOptions();
+        options.setExpandContext(context);
+
+        final RdfDataset result = JsonLd.toRdf(document).options(options).get();
+
+        assertNotNull(result);
+
+        try (final InputStream is = getClass().getResourceAsStream("issue61-out.nq")) {
             
             assertNotNull(is);
+
+        System.out.println("testToRdfMissingTriples2");
+        printResult(result);
 
             boolean match = RdfComparison.equals(result, RdfDocument.of(is).getRdfContent().orElse(null));
             
@@ -39,10 +69,47 @@ public class ToRdfIssue61Test {
         }
     }
 
+    @Test
+    public void testToRdfMissingTriples3() throws JsonLdError, IOException {
+
+        final String document = getTestURL("issue61-in.json");
+        final String context = getTestURL("issue61-context.json");
+        JsonLdOptions options = new JsonLdOptions();
+        options.setExpandContext(context);
+
+        final RdfDataset result = JsonLd.toRdf(document).options(options).get();
+
+        assertNotNull(result);
+
+        try (final InputStream is = getClass().getResourceAsStream("issue61-out.nq")) {
+
+            assertNotNull(is);
+
+        System.out.println("testToRdfMissingTriples3");
+        printResult(result);
+
+            boolean match = RdfComparison.equals(result, RdfDocument.of(is).getRdfContent().orElse(null));
+
+            assertTrue(match);
+        }
+    }
+
+    private final String getTestURL(String p) {
+      return "file://" + System.getProperty("user.dir") + "/src/test/resources/com/apicatalog/jsonld/issue/" + p;
+    }
+
+    private final void printResult(RdfDataset result) {
+      String res = "Size " + result.toList().size();
+      StringBuilder sb = new StringBuilder();
+      for ( RdfNQuad e : result.toList() )
+       sb.append(  "<" + e.getSubject() + "> <" + e.getPredicate() + "> " + e.getObject() ) . append("\n");
+      System.out.println(res + "\n" + sb);
+    }
+
     private final Document readDocument(final String name) throws JsonLdError, IOException {
         try (final InputStream is = getClass().getResourceAsStream(name)) {
             return JsonDocument.of(is);
         }
     }
-    
+
 }
