7eb6337bbf5fa34bb7c7a3e45a7c638f23053795
Filip <filip26@gmail.com>
2020-12-06 15:42:25 +0100

    improve test CompactBaseTest

--- src/test/java/com/apicatalog/jsonld/custom/CompactBaseTest.java
+++ src/test/java/com/apicatalog/jsonld/custom/CompactBaseTest.java
@@ -15,44 +15,27 @@
  */
 package com.apicatalog.jsonld.custom;
 
-import static org.junit.Assume.assumeTrue;
-
-import java.util.Collection;
-import java.util.stream.Collectors;
-
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 
 import com.apicatalog.jsonld.JsonLd;
-import com.apicatalog.jsonld.api.JsonLdError;
 import com.apicatalog.jsonld.document.JsonDocument;
 import com.apicatalog.jsonld.test.JsonLdManifestLoader;
 import com.apicatalog.jsonld.test.JsonLdTestCase;
 import com.apicatalog.jsonld.test.JsonLdTestRunnerJunit;
 import com.apicatalog.jsonld.test.loader.ZipResourceLoader;
 
-@RunWith(Parameterized.class)
 public class CompactBaseTest {
 
-    @Parameterized.Parameter(0)
-    public JsonLdTestCase testCase;
-
-    @Parameterized.Parameter(1)
-    public String testId;
-    
-    @Parameterized.Parameter(2)
-    public String testName;
-        
-    @Parameterized.Parameter(3)
-    public String baseUri;
-    
     @Test
     public void testCompactBase() {
 
-        // skip other tests
-        assumeTrue(testId.equals("#t0047"));
+        final JsonLdTestCase testCase = JsonLdManifestLoader
+                .load(JsonLdManifestLoader.JSON_LD_API_BASE, "compact-manifest.jsonld", new ZipResourceLoader())
+                .stream()  
+                .filter(o -> "#t0047".equals(o.id))
+                .findFirst().orElseThrow();
+                
         Assert.assertTrue(new JsonLdTestRunnerJunit(testCase).execute(options ->
 
                 JsonDocument.of(
@@ -60,15 +43,5 @@ public class CompactBaseTest {
                 )
 
         ));
-    }
-
-    @Parameterized.Parameters(name = "{1}: {2}")
-    public static Collection<Object[]> data() throws JsonLdError {        
-        return JsonLdManifestLoader
-                .load(JsonLdManifestLoader.JSON_LD_API_BASE, "compact-manifest.jsonld", new ZipResourceLoader())
-                .stream()  
-                .filter(o -> "#t0047".equals(o.id))
-                .map(o -> new Object[] {o, o.id, o.name, o.baseUri})
-                .collect(Collectors.toList());
     }
 }
