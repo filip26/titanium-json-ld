f0b391ed7a8704cea1c0f29f848b4ac8585f96c8
Jeremy Townson <jeremy.townson@gmail.com>
2021-01-19 12:01:10 +0100

    Remove dependencies on commons-lang and okhttp. This keeps the project dependency free.

--- src/main/java/com/apicatalog/jdk8/HttpResponse.java
+++ src/main/java/com/apicatalog/jdk8/HttpResponse.java
@@ -1,33 +1,37 @@
 package com.apicatalog.jdk8;
 
-import okhttp3.Response;
-
 import java.io.Closeable;
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.util.Collections;
 import java.util.List;
 
 public class HttpResponse implements Closeable {
 
-    private Response response;
+    private int statusCode;
+    private HttpURLConnection conn;
 
-    public HttpResponse(Response response) {
-        this.response = response;
+    public HttpResponse(int statusCode, HttpURLConnection conn) {
+        this.statusCode = statusCode;
+        this.conn = conn;
     }
 
     public int statusCode() {
-        return response.code();
+        return statusCode;
     }
 
     public List<String> headers(String h) {
-        return response.headers(h);
+        List<String> headerValues = conn.getHeaderFields().get(h);
+        return headerValues == null ? Collections.emptyList() : headerValues;
     }
 
-    public InputStream body() {
-        return response.body().byteStream();
+    public InputStream body() throws IOException {
+        return conn.getInputStream();
     }
 
     @Override
     public void close() {
-        response.close();
+        conn.disconnect();
     }
 }
