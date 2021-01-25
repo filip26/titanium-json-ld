5eb76beb3ee2644e3ecf2f73152fcd4dc451b8d6
Jeremy Townson <jeremy.townson@gmail.com>
2021-01-25 13:50:44 +0100

    Apply android compatible changes within android build profile.

---
+++ src/main/android/com/apicatalog/jsonld/loader/HttpClient.java
@@ -0,0 +1,22 @@
+package com.apicatalog.jsonld.loader;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HttpClient {
+
+    public static final int connectTimeoutMillis = 5000;
+    public static final int readTimeoutMillis = 5000;
+
+    public HttpResponse get(URL url, String accept) throws IOException {
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setConnectTimeout(connectTimeoutMillis);
+        conn.setReadTimeout(readTimeoutMillis);
+        conn.setInstanceFollowRedirects(false);
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Accept", accept);
+        int statusCode = conn.getResponseCode();
+        return new HttpResponse(statusCode, conn);
+    }
+}
---
+++ src/main/android/com/apicatalog/jsonld/loader/HttpResponse.java
@@ -0,0 +1,37 @@
+package com.apicatalog.jsonld.loader;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.util.Collections;
+import java.util.List;
+
+public class HttpResponse implements Closeable {
+
+    private int statusCode;
+    private HttpURLConnection conn;
+
+    public HttpResponse(int statusCode, HttpURLConnection conn) {
+        this.statusCode = statusCode;
+        this.conn = conn;
+    }
+
+    public int statusCode() {
+        return statusCode;
+    }
+
+    public List<String> headers(String h) {
+        List<String> headerValues = conn.getHeaderFields().get(h);
+        return headerValues == null ? Collections.emptyList() : headerValues;
+    }
+
+    public InputStream body() throws IOException {
+        return conn.getInputStream();
+    }
+
+    @Override
+    public void close() {
+        conn.disconnect();
+    }
+}
