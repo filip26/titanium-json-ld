13da1c4ba41d0336b65b5160b8f4b5bde05d7c05
Jeremy Townson <jeremy.townson@gmail.com>
2021-01-26 16:40:52 +0100

    Use OkHttpClient for DocumentLoader.

--- src/main/android/com/apicatalog/jsonld/loader/HttpClient.java
+++ src/main/android/com/apicatalog/jsonld/loader/HttpClient.java
@@ -1,22 +1,24 @@
 package com.apicatalog.jsonld.loader;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
 import java.io.IOException;
-import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class HttpClient {
 
-    public static final int connectTimeoutMillis = 5000;
-    public static final int readTimeoutMillis = 5000;
+    private OkHttpClient client = new OkHttpClient.Builder()
+            .followRedirects(false)
+            .followSslRedirects(false)
+            .build();
 
     public HttpResponse get(URL url, String accept) throws IOException {
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setConnectTimeout(connectTimeoutMillis);
-        conn.setReadTimeout(readTimeoutMillis);
-        conn.setInstanceFollowRedirects(false);
-        conn.setRequestMethod("GET");
-        conn.setRequestProperty("Accept", accept);
-        int statusCode = conn.getResponseCode();
-        return new HttpResponse(statusCode, conn);
+        Request request = new Request.Builder()
+                .header("Accept", accept)
+                .url(url)
+                .build();
+
+        return new HttpResponse(client.newCall(request).execute());
     }
 }
